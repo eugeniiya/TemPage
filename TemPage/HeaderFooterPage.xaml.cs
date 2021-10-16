7e18f9e00c6dfd9bedb4d58f8574ce850b87b534
eugeniiya <evgenia.fonina.2013@yandex.ru>
2021-10-16 12:52:10 +0300

    Добавьте файлы проекта.

---
+++ TemPage/HeaderFooterPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace TemPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HeaderFooterPage : ContentPage
+    {
+        ControlTemplate GrayTemplate;
+        ControlTemplate BlueTemplate;
+        public static readonly BindableProperty
+        HeaderTextProperty =
+        BindableProperty.Create(nameof(HeaderText),
+        typeof(string),
+        typeof(HeaderFooterPage),
+        default(string));
+        public string HeaderText
+        {
+            get => (string)GetValue(HeaderTextProperty);
+            set => SetValue(HeaderTextProperty, value);
+        }
+        bool original = true;
+        public bool OriginalTemplate
+        {
+            get { return original; }
+        }
+        public HeaderFooterPage()
+        {
+            InitializeComponent();
+            GrayTemplate =
+            (ControlTemplate)Resources["GrayTemplate"];
+            BlueTemplate =
+            (ControlTemplate)Resources["BlueTemplate"];
+        }
+        void OnChangeTheme(object sender, EventArgs e)
+        {
+            original = !original;
+            ControlTemplate =
+            (original) ? GrayTemplate : BlueTemplate;
+        }
+    }
+}
---
+++ TemPage/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TemPage
+{
+    public partial class MainPage : HeaderFooterPage
+    {
+        Label TemLabel;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            TemLabel = (Label)GetTemplateChild("СhangeThemeLabel");
+            TemLabel.Text =
+            OriginalTemplate ? "Gray Theme" : "Blue Theme";
+        }
+    }
+}
+
